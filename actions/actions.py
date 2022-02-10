d62bb2b06165dbdc7c0ac4edbaebc3d5cdda3c78
hieuvo <hieuvo@lecle.co.kr>
2022-02-10 22:06:21 +0700

    CB-7 Handle base API

--- actions/actions.py
+++ actions/actions.py
@@ -1,11 +1,1 @@
-# ? This file will execute first time launch config, etc.
-# ! Only put first time launch config here
 
-
-class App:
-    @staticmethod
-    def build():
-        print("I love mom")
-
-
-App.build()
--- utils/chatbot_config.py
+++ utils/chatbot_config.py
@@ -5,10 +5,6 @@ from utils.global_configuration import GlobalConfiguration
 class AppConfig:
     global_configuration: GlobalConfiguration = GlobalConfiguration()
 
-    # @param ClassMethod
-    # ? được dùng để định nghĩa class này là class constants
-    # ? dùng cho mấy hàm constants khi muốn truy cập vào biến của class
-
     @classmethod
     def load_config(cls):
         cls.global_configuration.set_config()
