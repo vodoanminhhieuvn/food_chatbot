43f6db2c39877d15945ff8d20164019341f3cec8
khangle <khangle@lecle.co.kr>
2022-02-12 17:22:40 +0700

    CB-9 Refactor Ingredient

--- base_models/rasa_model/slot_model.py
+++ base_models/rasa_model/slot_model.py
@@ -1,19 +1,18 @@
 from typing import List
 
 from base_models.food_model.nutrient_model import NutrientModel
+from base_models.food_model.recipe_part_model import RecipePartsModel
 
 
 class Slot:
     target_calory: int = 2000
     diet: str = ""
     nutrient_slots = NutrientModel()
-    list_ingredients: List[str]
+    recipe_parts_slots = RecipePartsModel()
+    recipe_search_keyword_slots: list = []
 
     def set_nutrient_attr(self, name: str, value: int):
         self.nutrient_slots.__setattr__(name, value)
-
-    def set_list_ingredient(self, list_ingredient: List[str]):
-        self.list_ingredients = list_ingredient
 
 
 slot = Slot()
