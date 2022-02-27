cb3482e02b0be9b6add34523b8508693142129b1
hieuvo <hieuvo@lecle.co.kr>
2022-02-27 10:52:45 +0700

    CB-11 Migrate API to actions

--- actions/food_actions/recipe_actions.py
+++ actions/food_actions/recipe_actions.py
@@ -2,8 +2,29 @@ from typing import Text, Dict, Any, List
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.types import DomainDict
+from requests import Response
 
+from api.get_api import GetApi, EdamamMethod, get_food_recipe
+from base_models.rasa_model.slot_model import slot
 from data.responses.food_reponses.recipe_responses import RecipeResponses
+
+
+class ActionGetRecipe(Action):
+    def name(self) -> Text:
+        return "action_get_recipe"
+
+    def run(
+            self,
+            dispatcher: "CollectingDispatcher",
+            tracker: Tracker,
+            domain: DomainDict,
+    ) -> List[Dict[Text, Any]]:
+        response: Response = GetApi.request_edamam(EdamamMethod.GET_RECIPES,
+                                                   params=get_food_recipe("Chicken", nutrient_info=slot.nutrient_slots))
+
+        print(response)
+        return []
 
 
 class ActionHelpGetRecipe(Action):
--- api/get_api.py
+++ api/get_api.py
@@ -19,14 +19,14 @@ class SpooncularMethod(enum.Enum):
 
 class GetApi:
     @staticmethod
-    def request_edamam(edamam_method: EdamamMethod):
+    def request_edamam(edamam_method: EdamamMethod, params: Dict[str, any]) -> Response:
         response: Response
 
         if edamam_method == EdamamMethod.GET_RECIPES:
-            response = NetworkUtil.request(GlobalConfiguration.edamam_api_config.searchUrl, params=[],
+            response = NetworkUtil.request(GlobalConfiguration.edamam_api_config.searchUrl, params=params,
                                            method=NetworkMethod.GET)
         else:
-            response = NetworkUtil.request(GlobalConfiguration.spooncular_api_config.generateMealUrl, params=[],
+            response = NetworkUtil.request(GlobalConfiguration.edamam_api_config.searchUrl, params=params,
                                            method=NetworkMethod.GET)
 
         return response
@@ -45,7 +45,7 @@ class GetApi:
         return response
 
 
-def load_edamam_config() -> Dict[str, str]:
+def load_edamam_config() -> Dict[str, any]:
     return {
         "app_id": GlobalConfiguration.edamam_api_config.id,
         "app_key": GlobalConfiguration.edamam_api_config.key,
@@ -53,7 +53,23 @@ def load_edamam_config() -> Dict[str, str]:
     }
 
 
-def load_spooncular_config() -> Dict[str, str]:
+def load_spooncular_config() -> Dict[str, any]:
     return {
         "apiKey": GlobalConfiguration.spooncular_api_config.key,
+    }
+
+
+def get_food_recipe(search_params: str, nutrient_info: NutrientModel) -> Dict[str, any]:
+    return {
+        "q": search_params,
+        "calories": f"{nutrient_info.minCalories}-{nutrient_info.maxCalories}",
+        "fat": f"{nutrient_info.minFat}-{nutrient_info.maxFat}"
+    }
+
+
+def get_meal_plan() -> Dict[str, any]:
+    return {
+        "query": "Query",
+        "targetCalories": 2000,
+        "diet": "Vegan",
     }
